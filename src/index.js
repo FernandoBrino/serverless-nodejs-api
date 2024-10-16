3f751d291a4f06df1018284a5fa896bd5cff0d1a
Fernando Brino <fernandobrino11@gmail.com>
2024-10-16 17:12:17 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 const serverless = require("serverless-http");
 const express = require("express");
-const { neon, neonConfig } = require('@neondatabase/serverless');
+const { neon, neonConfig } = require("@neondatabase/serverless");
+const AWS = require("aws-sdk");
+
+const AWS_REGION = "us-east-1";
+const STAGE = process.env.STAGE || 'prod';
+
 const app = express();
+const ssm = new AWS.SSM({ region: AWS_REGION });
+
+//Parameter name inside AWS SSM
+const DATABASE_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`;
 
 async function dbClient() {
+
+  // Get parameter "database url" inside ssm from amazon
+  const paramStoreData = await ssm.getParameter({
+    Name: DATABASE_URL_SSM_PARAM,
+    WithDecryption: true
+  }).promise()
+
+  // Get database url
+  const dbUrl = paramStoreData.Parameter.Value
+
   // for http connection
   // non-pooling
   neonConfig.fetchConnectionCache = true;
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(dbUrl);
   return sql;
 }
 
 app.get("/", async (req, res, next) => {
   const sql = await dbClient();
-  const [results] = await sql`select now();`
+  const [results] = await sql`select now();`;
 
   return res.status(200).json({
     message: "Hello from root!",
-    results: results.now
+    results: results.now,
   });
 });
 
@@ -32,7 +51,6 @@ app.use((req, res, next) => {
     error: "Not Found",
   });
 });
-
 
 // server-full app
 // app.listen(3000, () => {
